6f2e48778c0197a5ddd2fa8a3487b001a68e2d15
workzyn <zhuynchn@163.com>
2018-08-21 09:48:09 +0800

    exportOptionsPlist

--- AutoPackage-sh/autoPackage.sh
+++ AutoPackage-sh/autoPackage.sh
@@ -7,7 +7,7 @@ git pull
 
 xcodebuild -workspace /Users/xxx/xxx.xcworkspace -scheme xxx -destination generic/platform=iOS archive -configuration Release ONLY_ACTIVE_ARCH=NO -archivePath /Users/xxx/AutoPackage/xcarchive/xxx_$DATE
 
-xcodebuild -exportArchive -archivePath /Users/xxx/AutoPackage/xcarchive/xxx_$DATE.xcarchive -exportPath /Users/xxx/AutoPackage/ipa/ipa_$DATE -exportOptionsPlist /Users/xxx/AutoPackage/exportOptionsPlist/exportOptionsPlist.plist
+xcodebuild -exportArchive -archivePath /Users/xxx/AutoPackage/xcarchive/xxx_$DATE.xcarchive -exportPath /Users/xxx/AutoPackage/ipa/ipa_$DATE -exportOptionsPlist /Users/xxx/AutoPackage/c/exportOptionsPlist.plist
 
 fir login -T 20da9c980e365eb85eca2cf464748a7a
 
